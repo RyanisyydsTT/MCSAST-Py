87009c88eb95449f5f83fc87537b8eea9f0c4955
Ryanisyyds <129717677+RyanisyydsTT@users.noreply.github.com>
2024-06-04 19:27:41 +0800

    Update code_def.py
    fix some bug

--- code_def.py
+++ code_def.py
@@ -308,17 +308,18 @@ def setup_van_server():
             file = open('runserver.bat', 'a')
             file.write(runscipt)
             file.close()
-        else:
+        elif Aikar == "n" or "N" and system == "Windows":
             runscript = "java -Xms4096M -Xmx4096M --add-modules=jdk.incubator.vector -jar server.jar --nogui"
             file = open('runserver.bat', 'a')
             file.write(runscipt)
             file.close()    
-        if Aikar == "y" or "Y" and system == "Linux":
+        if Aikar == "y" or "Y" and system == "Linux" or "OSX":
+            # 
             runscipt = "java -Xms4096M -Xmx4096M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar --nogui"
             file = open('runserver.sh', 'a')
             file.write(runscipt)
             file.close()    
-        else:
+        elif Aikar == "n" or "N" and system == "Linux" or "OSX":
             runscript = "java -Xms4096M -Xmx4096M --add-modules=jdk.incubator.vector -jar server.jar --nogui"
             file = open('runserver.sh', 'a')
             file.write(runscipt)
